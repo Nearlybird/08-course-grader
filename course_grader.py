12378bc33da61377b9e0aa6d50fd395b2013eda7
Nearlybird <49175582+Nearlybird@users.noreply.github.com>
2019-04-21 12:28:11 -0600

    Update course_grader.py

--- course_grader.py
+++ course_grader.py
@@ -1,2 +1,18 @@
-def course_grader(param):
-    return ""
+def course_grader(test_scores):
+    ave_score = sum(test_scores)/int(len(test_scores))
+    output = 0
+    
+    if ave_score < 70:
+        output = output + 1
+        
+    elif ave_score >= 70:
+        for score in (test_scores):
+            if score < 50:
+                output = output + 1
+            else:
+                output = output
+    
+    if output > 0:
+        return "fail"
+    else:
+        return "pass"
